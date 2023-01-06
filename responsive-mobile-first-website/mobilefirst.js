e611e12d722c5c6aad3f44f9b87e5288ae7f20e8
“Sarmistha <sarmisthasantikari@gmail.com>
2023-01-06 20:39:23 +0530

    syntax correction in js file

--- responsive-mobile-first-website/mobilefirst.js
+++ responsive-mobile-first-website/mobilefirst.js
@@ -1,6 +1,7 @@
 $('#view-work').on('click' ,function() {
     const images = $('#images').position().top;
+    console.log("images", images);
     $('html,body').animate({
-        screenTop :images
-    },900);
+        scrollTop :images
+    }, 1000);
 });
